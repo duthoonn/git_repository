51eb4ccbb518f9f99dda44819a449161f31e0356
duthoonn <65177192+duthoonn@users.noreply.github.com>
2020-05-13 22:45:28 +0800

    Update Test1.java

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -1,7 +1,7 @@
 package com.usian.java;
 
 public class Test1 {
-    //修改test1
+    //远程github仓库中的代码
     public static void main(String[] args) {
         //github端其他同事的修改
         System.out.println("师姐你好");
