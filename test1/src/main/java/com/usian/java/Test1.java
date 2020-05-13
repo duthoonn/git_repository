ec00439a61fc8d6d65c6958abffcdd1ce40292e9
duthoonn <1694998916@qq.com>
2020-05-13 23:51:17 +0800

    修改test1

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -1,9 +1,8 @@
 package com.usian.java;
 
 public class Test1 {
-    //远程github仓库中的代码
     public static void main(String[] args) {
         //github端其他同事的修改
-        System.out.println("师姐你好");
+        System.out.println("修改1.0分支的bug");
     }
 }
