c1a756a1ecaf9e0500c61c940bdad14f18dfbcdf
duthoonn <1694998916@qq.com>
2020-05-13 23:59:33 +0800

    修改test1

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -3,6 +3,6 @@ package com.usian.java;
 public class Test1 {
     public static void main(String[] args) {
         //github端其他同事的修改
-        System.out.println("修改1.0分支的bug");
+        System.out.println("修改1.0的bug");
     }
 }
